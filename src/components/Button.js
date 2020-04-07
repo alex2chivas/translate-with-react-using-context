5cf635fc4a5f3d6a6eb66cd5bf032fbdaabdf95b
Alexis Flores <alex2chivas@hotmail.com>
2020-04-07 14:13:24 -0700

    changed names to make it easier to read data and implemented deconstruction since we are workign with objects now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
-import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
 	// Note - ContextType property is a sepecial name. Do not change
 	// Note - This is the comsumer approach!
-	renderSubmit(value) {
-		return value === 'english' ? 'Submit' : 'Voorleggen';
+	renderSubmit(language) {
+		return language === 'english' ? 'Submit' : 'Voorleggen';
 	}
 
 	renderButton(color) {
 		return (
 			<button className={`ui button ${color}`}>
-				<LanguageContext.Consumer>{value => this.renderSubmit(value)}</LanguageContext.Consumer>
+				<LanguageContext.Consumer>
+					{({ language }) => this.renderSubmit(language)}
+				</LanguageContext.Consumer>
 			</button>
 		);
 	}
 
 	render() {
-		return <ColorContext.Consumer>{color => this.renderButton(color)}</ColorContext.Consumer>;
+		return (
+			<LanguageContext.Consumer>{({ color }) => this.renderButton(color)}</LanguageContext.Consumer>
+		);
 	}
 }
 

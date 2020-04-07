2c9e2fa0c3b58c5a686536ec0d38580414078514
Alexis Flores <alex2chivas@hotmail.com>
2020-04-07 07:43:32 -0700

    added clicker handlers to toggle between english and dutch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import UserCreate from './UserCreate';
+
 class App extends Component {
+	state = { language: 'english' };
+
+	onLanguageChange = language => {
+		this.setState({ language });
+	};
+
 	render() {
 		return (
 			<div className='ui container'>
-				<div>App</div>
+				<div>
+					Select a language
+					<i onClick={() => this.onLanguageChange('english')} className='flag us' />
+					<i onClick={() => this.onLanguageChange('dutch')} className='flag nl' />
+				</div>
+				<UserCreate />
 			</div>
 		);
 	}

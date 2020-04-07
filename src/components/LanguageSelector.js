47764af49f728c4828830693fe0ffe76f7b23704
Alexis Flores <alex2chivas@hotmail.com>
2020-04-07 14:15:05 -0700

    converted this.props on the i tags into this.context since we have a central store now handled by LanguageContext

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
+import LanguageContext from '../contexts/LanguageContext';
+
 class LanguageSelector extends Component {
+	static contextType = LanguageContext;
+
 	render() {
+		console.log(this.context);
 		return (
 			<div>
 				<div style={{ display: 'flex' }}>
 					Select a language:
 					<div style={{ marginLeft: '10px' }}>
 						<i
-							onClick={() => this.props.onLanguageChange('english', 'primary')}
+							onClick={() => this.context.onLanguageChange('english', 'primary')}
 							className='flag us'
 						/>
-						<i onClick={() => this.props.onLanguageChange('dutch', 'red')} className='flag nl' />
+						<i onClick={() => this.context.onLanguageChange('dutch', 'red')} className='flag nl' />
 					</div>
 				</div>
 			</div>
